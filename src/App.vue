3544063b1248a4aa717df150c21b3212a8fc317e
nikolaychirkov <nikolay4irkov@gmail.com>
2020-05-06 22:29:32 +0300

    history mode for router enabled, dynamic route created, some other minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'event-list' }">EventList</router-link>|
-      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link>|
+      <router-link :to="{ name: 'event-list' }">List</router-link>|
       <router-link :to="{ name: 'event-create' }">EventCreate</router-link>
     </div>
     <router-view />
