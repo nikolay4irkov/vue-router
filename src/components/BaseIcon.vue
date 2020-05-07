a06f73f4fda48d77bd2ebc7fcdf823a44f7082e6
nikolaychirkov <nikolay4irkov@gmail.com>
2020-05-07 23:03:00 +0300

    svg sprite added, automatic global component registration added, baseIcon component created

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,6 +3,7 @@
     <div class="event-card">
       <span>@ {{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
