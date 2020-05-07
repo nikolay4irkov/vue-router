f857c8f347f5cfeaacad053c5416af9e52a8fb2d
nikolaychirkov <nikolay4irkov@gmail.com>
2020-05-07 20:13:03 +0300

    event card component done

---
+++ src/components/EventCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
+    <div class="event-card">
+      <span>@ {{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: "Beach Cleanup",
+        date: "Tues Aug 19, 2019",
+        time: "6:00",
+        attendees: [
+          { id: "abc123", name: "Adam Jahr" },
+          { id: "def456", name: "Gregg Pollack" }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.event-card {
+  width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  margin-bottom: 24px;
+  cursor: pointer;
+  border: 1px solid gray;
+  transition: all 0.2s;
+  text-align: left;
+
+  &:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2),
+      0 1px 15px 0 rgba(0, 0, 0, 0.19);
+  }
+}
+
+.event-card > .title {
+  margin: 0;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.event-link {
+  color: black;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/">Real World Vue</router-link>|
+    <nav>
+      <router-link :to="{ name: 'event-list' }">List</router-link>|
+      <router-link :to="{ name: 'event-create' }">EventCreate</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
