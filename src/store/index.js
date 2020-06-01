7e3d08566dd8aa4687490283f840280646309fe8
nikolaychirkov <nikolay4irkov@gmail.com>
2020-06-01 23:50:31 +0300

    added pagination for event list, enabled caching for events (in order not to make a call to the API twice)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import EventService from '@/services/EventService.js';
+import EventService from "@/services/EventService.js";
 
 Vue.use(Vuex);
 
@@ -16,24 +16,60 @@ export default new Vuex.Store({
       "food",
       "community"
     ],
-    events: []
+    events: [],
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal;
+    },
+    SET_EVENT(state, event) {
+      state.event = event;
     }
   },
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
-      })
-
+      });
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit(
+            "SET_EVENTS_TOTAL",
+            parseInt(response.headers["x-total-count"])
+          );
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log("There was an error:" + error.response);
+        });
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id);
+      if (event) {
+        commit("SET_EVENT", event);
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit("SET_EVENT", response.data);
+          })
+          .catch(error => {
+            console.log("There was an error", error.response);
+          });
+      }
     }
   },
   getters: {
-    catLength: state => {
-      return state.categories.length;
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id);
     }
   },
   modules: {}
