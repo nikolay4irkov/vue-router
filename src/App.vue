ea2539c1c2182d18706376727b2f5af549f042a1
nikolaychirkov <nikolay4irkov@gmail.com>
2020-06-11 11:51:41 +0300

    notification module for displaing success and error notifications added

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <NavBar />
     <router-view :key="$route.fullPath" />
+    <NotificationContainer />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import NotificationContainer from "@/components/NotificationContainer.vue";
 export default {
   components: {
-    NavBar
+    NavBar,
+    NotificationContainer
   }
 };
 </script>
